ee75b705a388452586460e5784dde005f3a7fa0b
Sergio Trigueros <34091898+triguii@users.noreply.github.com>
2026-03-08 23:48:11 +0900

    button

--- objects/UI/Button.lua
+++ objects/UI/Button.lua
@@ -2,7 +2,7 @@ _G.love = require("love")
 ---@diagnostic disable: lowercase-global
 
 
-function Button (func, x, y, width, height, text, text_color, botton_color)
+function Button (func, x, y, width, height, text, text_color, button_color)
 
     local btn_text = {}
 
@@ -16,7 +16,10 @@ function Button (func, x, y, width, height, text, text_color, botton_color)
         height = height,
         text = text, 
         text_color = text_color,
-        botton_color = botton_color,
+        button_color = button_color,
+        text_component = love.graphics.newText( FONT,  text),
+        text_offset = 10,
+
 
         checkHover = function (self, mouse_x, mouse_y, cursor_radius)
             if (mouse_x + cursor_radius >= self.x) and (mouse_x - cursor_radius <= self.x + self.width) then
@@ -28,6 +31,10 @@ function Button (func, x, y, width, height, text, text_color, botton_color)
             return false
         end,
 
+        setButtonColor = function (self, red, green, blue)
+            self.button_color = { r = red, g = green, b = blue}
+        end,
+
         click = function (self)
             self.func()
         end,
@@ -35,9 +42,11 @@ function Button (func, x, y, width, height, text, text_color, botton_color)
         draw = function (self)
             love.graphics.setColor(self.button_color["r"], self.button_color["g"], self.button_color["b"])
             love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
-        
-            self.text_component:setColor(self.text_color["r"], self.text_color["g"], self.text_color["b"])
-            self.text_component:draw()
+
+
+            love.graphics.setColor(self.text_color["r"], self.text_color["g"], self.text_color["b"])
+            love.graphics.draw(self.text_component, self.x + self.text_offset, self.y + (self.height/2) - 5)
+
         
             love.graphics.setColor(1, 1, 1)
         end,
--- objects/UI/UIClickMenu.lua
+++ objects/UI/UIClickMenu.lua
@@ -1,15 +1,20 @@
 ---@diagnostic disable: lowercase-global
 _G.love = require("love")
 
+local Button = require('objects.UI.Button')
+
+
 function UIClickMenu()
     local mouseCoordX, mouseCoordY = love.mouse.getPosition()
+    mouseCoordX, mouseCoordY = cam:worldCoords(mouseCoordX, mouseCoordY)
 
     return {
-        height = SPRITE_SIZE * 3,
-        width = SPRITE_SIZE * 5,
+        height = SPRITE_SIZE * 2,
+        width = SPRITE_SIZE * 3,
         x_pos = mouseCoordX,
         y_pos = mouseCoordY,
         radius = 10,
+        
 
         drawEmptyMenu = function (self)
             --Set Base
@@ -25,6 +30,18 @@ function UIClickMenu()
             love.graphics.setColor(1, 1, 1, 1)
 
             --Set buttons
+
+            local EndTurnButton = Button(nil, self.x_pos + 10, self.y_pos + 10, SPRITE_SIZE * 2, SPRITE_SIZE, "End Turn", {r = 0, g = 0, b = 0}, {r = 0.7, g = 0.7, b = 0.7})
+
+            local mouseCoordX, mouseCoordY = love.mouse.getPosition()
+            mouseCoordX, mouseCoordY = cam:worldCoords(mouseCoordX, mouseCoordY)
+
+            if EndTurnButton:checkHover(mouseCoordX, mouseCoordY, 4) then
+                EndTurnButton:setButtonColor(0.5, 0.5, 0.5)
+            end
+
+            EndTurnButton:draw()
+            
             
         end
     }
